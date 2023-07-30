0ee34d288d5c081a15679fbfd0f76f0c97194dd9
aikhsanov <adrstrings@gmail.com>
2023-07-30 14:33:47 +0300

    +add login logic in auth controller !wip me logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -23,3 +23,45 @@ exports.register = async (req, res) => {
     res.status(500).json({ error: e });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!existingUser) throw new Error("User with given email is not found");
+    bcrypt.compare(password, existingUser?.password, (err, match) => {
+      if (err) throw new Error(err);
+      if (match) {
+        res
+          .status(200)
+          .json({ token: generateToken(existingUser), user: existingUser });
+        return;
+      }
+      res.status(403).json({ error: "passwords do not match" });
+    });
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+};
+
+exports.me = async (req, res) => {
+  try {
+    await User.findOne({
+      where: { token: req?.headers?.authorization },
+    })
+      .then((user) => {
+        if (user) {
+          res.status(200).json({ data: { user: user } });
+        }
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
