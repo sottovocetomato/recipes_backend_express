279ad1aee277b660934470af503895f620cad877
ai-strings <adrstrings@gmail.com>
2023-08-10 16:27:33 +0300

    !fix filter function

--- helpers/filter.js
+++ helpers/filter.js
@@ -5,9 +5,13 @@ exports.parseFilter = (filter) => {
   for (const key in filter) {
     const index = filter[key].indexOf("(");
     if (!index) throw new Error("Wrong filter format");
+
     const rule = filter[key].slice(0, index);
-    const val = filter[key].slice(index + 1, filter[key].indexOf(")"));
-    where[key] = { [Op[rule.toLowerCase()]]: `%${val}` };
+    let val = filter[key].slice(index + 1, filter[key].indexOf(")"));
+
+    if (rule === "LIKE") val = `%${val}%`;
+
+    where[key] = { [Op[rule.toLowerCase()]]: val };
   }
   return where;
 };
