f06c6f8443199c1427c782725a0829cdb63e3e49
ai-strings <adrstrings@gmail.com>
2023-09-25 22:03:04 +0300

    !wip recipe.model

--- models/recipe_step.model.js
+++ models/recipe_step.model.js
@@ -1,22 +1,22 @@
-const {Sequelize, DataTypes, Model} = require("sequelize");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 module.exports = (sequelize, Sequelize) => {
-    const RecipeStep = sequelize.define(
-        "recipe_step",
-        {
-            description: {
-                type: DataTypes.STRING,
-            },
-            order: {
-                type: DataTypes.INTEGER,
-            },
-            img_url: {
-                type: DataTypes.STRING,
-            },
-        },
-        {
-            timestamps: false,
-        }
-    );
+  const RecipeStep = sequelize.define(
+    "recipe_step",
+    {
+      description: {
+        type: DataTypes.TEXT("long"),
+      },
+      order: {
+        type: DataTypes.INTEGER,
+      },
+      img_url: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
-    return RecipeStep;
+  return RecipeStep;
 };
