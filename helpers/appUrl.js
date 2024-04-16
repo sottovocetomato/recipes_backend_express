ce6703182d9a9426192f85660faaf1df1e4de52a
ai-strings <adrstrings@gmail.com>
2024-04-16 15:43:33 +0300

    !fix package.json scripts

--- helpers/appUrl.js
+++ helpers/appUrl.js
@@ -1,4 +1,5 @@
-exports.appUrl =
-  process.env.NODE_ENV === "dev"
-    ? "http://localhost:3000/"
-    : "http://localhost:5000/";
+// exports.appUrl =
+//   process.env.NODE_ENV === "dev"
+//     ? "http://localhost:3000/"
+//     : "http://localhost:5000/";
+exports.appUrl = "http://localhost:5000/";
