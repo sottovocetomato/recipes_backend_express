4044fbcc3921faca3d2dd54c7a2d2c14eab4c84d
ai-strings <adrstrings@gmail.com>
2024-04-17 13:35:44 +0300

    !updated server

--- server.js
+++ server.js
@@ -28,30 +28,31 @@ app.use(express.static(__dirname + "/public"));
 // app.use(cors(corsOptions));
 app.use(cors());
 app.options("*", cors());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  // res.setHeader("Access-Control-Allow-Origin", "http://receipts.haemmid.ru");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   // res.setHeader("Access-Control-Allow-Origin", "http://receipts.haemmid.ru");
+//
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.use(
   express.urlencoded({
