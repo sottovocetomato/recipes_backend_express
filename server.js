570b9f3a47cc913a45e8b9eedc7e6b3e65da38ce
ai-strings <adrstrings@gmail.com>
2024-04-17 14:36:38 +0300

    !updated server

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(cors());
 app.options("*", cors());
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader("Access-Control-Allow-Origin", "http://receipts.haemmid.ru");
 
   // Request methods you wish to allow
