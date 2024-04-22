5ed691a1e17f484c6ea316d4589276933a9fe37f
ai-strings <adrstrings@gmail.com>
2024-04-22 14:34:38 +0300

    !change app url

--- helpers/appUrl.js
+++ helpers/appUrl.js
@@ -2,5 +2,5 @@
 //   process.env.NODE_ENV === "dev"
 //     ? "http://localhost:3000/"
 //     : "http://localhost:5000/";
-exports.appUrl = "http://localhost:5000/";
-// exports.appUrl = "http://62.217.178.4/recipes-server/";
+// exports.appUrl = "http://localhost:5000/";
+exports.appUrl = "http://62.217.178.4/recipes-server/";
